64a408b971d1427280d2b13372af36a6cc68bd3a
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-11 14:26:40 +0800

    用clion写的c++程序
    用clion写的c++程序

---
+++ Clion-cpp/new01/cmake-build-debug/CMakeFiles/new01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new01.dir/main.cpp.obj"
+  "libnew01.dll.a"
+  "new01.exe"
+  "new01.exe.manifest"
+  "new01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
