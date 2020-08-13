e383bda3d203825823e6725d18b4c9990fb2af65
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-13 14:00:59 +0800

    用clion写的c++程序
    用clion写的c++程序

---
+++ Clion-cpp/chartype/cmake-build-debug/CMakeFiles/chartype.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chartype.dir/main.cpp.obj"
+  "chartype.exe"
+  "chartype.exe.manifest"
+  "chartype.pdb"
+  "libchartype.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chartype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Clion-cpp/chartype/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    char ch;
+    int ch1;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+    ch1 = ch;
+    cout << "Thank you for the " << ch1 << " ." << endl;
+    cout << "Thank you for the" << ch << " character." << endl;
+    return 0;
+}
