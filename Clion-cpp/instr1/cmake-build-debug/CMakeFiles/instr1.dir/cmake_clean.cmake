d429d8e4945623ebd4d9fecd72a3edb1cf0842a5
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-22 20:46:28 +0800

    用clion写的c++程序
    用clion写的c++程序

---
+++ Clion-cpp/instr1/cmake-build-debug/CMakeFiles/instr1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/instr1.dir/main.cpp.obj"
+  "instr1.exe"
+  "instr1.exe.manifest"
+  "instr1.pdb"
+  "libinstr1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/instr1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
