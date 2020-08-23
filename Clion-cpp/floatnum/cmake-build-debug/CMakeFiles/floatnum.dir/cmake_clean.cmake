116016f9a54d26d033b8537069c7f8c974176878
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-23 20:35:26 +0800

    用clion写的c++程序
    用clion写的c++程序

---
+++ Clion-cpp/floatnum/cmake-build-debug/CMakeFiles/floatnum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/floatnum.dir/main.cpp.obj"
+  "floatnum.exe"
+  "floatnum.exe.manifest"
+  "floatnum.pdb"
+  "libfloatnum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/floatnum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
