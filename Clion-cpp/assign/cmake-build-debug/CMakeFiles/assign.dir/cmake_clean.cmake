4ba84eba78acfafe5c6e010d84e25851d7f7c9e2
FAN3XD <57396519+FAN3XD@users.noreply.github.com>
2020-08-25 22:49:01 +0800

    用clion写的c++程序
    用clion写的c++程序

---
+++ Clion-cpp/assign/cmake-build-debug/CMakeFiles/assign.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assign.dir/main.cpp.obj"
+  "assign.exe"
+  "assign.exe.manifest"
+  "assign.pdb"
+  "libassign.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assign.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Clion-cpp/assign/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout.setf(ios_base::fixed,ios_base::floatfield);
+    float tree = 3;
+    int guess(3.9832);
+    int debt = 7.2E12;
+    cout << "tree = " << tree << endl;
+    cout << "guess = " << guess << endl;
+    cout << "debt = " << debt << endl;
+
+    return 0;
+}
